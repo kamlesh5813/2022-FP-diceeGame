f199ab3a1af85947f77526cab968e49d6674b500
Kamlesh Vetal <86909725+kamlesh5813@users.noreply.github.com>
2022-01-11 13:35:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomImg1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src" , randomImg1);
+
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomImg2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src" , randomImg2);
+
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
